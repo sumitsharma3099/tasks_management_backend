d99d0578a8a68282449ce6e3e0347a6bf2d0fc6c
Sumit <ss828863@gmail.com>
2024-01-29 12:26:16 +0530

    setup cors for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://tasks-management-backend.onrender.com"
+    origins "https://tasks-management-frontend.onrender.com"
 
     resource "*",
       headers: :any,
